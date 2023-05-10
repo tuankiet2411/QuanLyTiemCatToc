4a9bbe5bea5f00b9130fa77a5cbbc13aad239139
Kiethehe <kietcon12347@gmail.com>
2023-05-10 09:01:21 +0700

    update project

--- src/Application/application.java
+++ src/Application/application.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Test;
+package Application;
 
 import TiemCatToc.View.TiemCatTocView;
 
@@ -10,7 +10,7 @@ import TiemCatToc.View.TiemCatTocView;
  *
  * @author MSI PC
  */
-public class test {
+public class application {
     public static void main(String[] args) {
         new TiemCatTocView();
     }
